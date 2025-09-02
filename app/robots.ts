acaeaf516f2a43102ae816c0e14d099bbd53c88a
Tref <nick@treflabs.com>
2025-09-01 21:50:26 -0400

    adding robots.txt

--- app/robots.ts
+++ app/robots.ts
@@ -1,12 +1,11 @@
-import { baseUrl } from 'app/sitemap'
+import type { MetadataRoute } from "next";
+import { SITE_URL } from "@/lib/site";
 
-export default function robots() {
+export default function robots(): MetadataRoute.Robots {
+  const base = SITE_URL || "https://www.nicktreffiletti.com";
   return {
-    rules: [
-      {
-        userAgent: '*',
-      },
-    ],
-    sitemap: `${baseUrl}/sitemap.xml`,
-  }
+    rules: [{ userAgent: "*", allow: "/" }],
+    sitemap: `${base}/sitemap.xml`,
+    host: "https://www.nicktreffiletti.com",
+  };
 }
