f09582d13d6bb48a22a79c9d4ed5c3bb2ff182b5
Tref <nick@treflabs.com>
2025-09-01 20:43:04 -0400

    adding head.tsx for the second time

---
+++ app/head.tsx
@@ -0,0 +1,21 @@
+// app/head.tsx
+export default function Head() {
+  const jsonLd = {
+    "@context": "https://schema.org",
+    "@type": "Person",
+    name: "Nick Treffiletti",
+    alternateName: "Nicholas Treffiletti",
+    url: "https://www.nicktreffiletti.com",
+    sameAs: ["https://linkedin.com/in/nicktreffiletti", "https://github.com/treffiletti", "https://medium.com/@newyorknick"],
+  };
+
+  return (
+    <>
+      <script
+        type="application/ld+json"
+        // IMPORTANT: raw JSON, not escaped
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+      />
+    </>
+  );
+}
