57ae880bc8f94c39abff8ba75fed88b715f572b1
Tref <nick@treflabs.com>
2025-09-01 20:27:32 -0400

    adding head jsonld

--- app/layout.tsx
+++ app/layout.tsx
@@ -55,7 +55,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" className={cx("text-black bg-white dark:text-white dark:bg-black", GeistSans.variable, GeistMono.variable)}>
       <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
-        <Script
+        {/* <Script
           id="person-jsonld"
           type="application/ld+json"
           dangerouslySetInnerHTML={{
@@ -73,7 +73,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
               ],
             }),
           }}
-        />
+        /> */}
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
           <Navbar />
           {children}
