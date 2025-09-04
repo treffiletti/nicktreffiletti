e721ca4ef7aa22c3ccea47249a5f56d359c246ca
Nick (Nicholas) Treffiletti <10848+treffiletti@users.noreply.github.com>
2025-09-03 20:24:53 -0400

    Fix sitemap (#8)
    * using BEEHIIV_API_KEY from const not env
    
    * adding vercel site_url

--- app/api/subscribe/route.ts
+++ app/api/subscribe/route.ts
@@ -21,7 +21,7 @@ export async function POST(req: Request) {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
-            Authorization: `Bearer ${process.env.BEEHIIV_API_KEY!}`,
+            Authorization: `Bearer ${BEEHIIV_API_KEY}`,
           },
           body: JSON.stringify({
             email,
