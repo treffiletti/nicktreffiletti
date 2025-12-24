8927d2194a45b4381eb43a4dfd2fd5e342e42ae2
Tref <nick@treflabs.com>
2025-12-24 00:02:45 -0500

    updating photo on Overview

--- src/app/(sidebar)/page.tsx
+++ src/app/(sidebar)/page.tsx
@@ -51,7 +51,7 @@ export default async function Page() {
         <div className="absolute -inset-x-2 top-0 -z-10 h-80 overflow-hidden rounded-t-2xl mask-b-from-60% sm:h-88 md:h-112 lg:-inset-x-4 lg:h-128">
           <img
             alt=""
-            src="https://assets.tailwindcss.com/templates/compass/hero-background.png"
+            src="/images/nick-Treffiletti-hero.jpg"
             className="absolute inset-0 h-full w-full mask-l-from-60% object-cover object-center opacity-40"
           />
           <div className="absolute inset-0 rounded-t-2xl outline-1 -outline-offset-1 outline-gray-950/10 dark:outline-white/10" />
